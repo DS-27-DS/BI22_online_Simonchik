9f88adde56ad2d3e9ccc68bf236d17dbb285d528
DS-27-DS <ss.dash@yandex.ru>
2024-06-03 20:35:30 +0300

    Задание 1. Знакомство с BI

---
+++ ДЗ_1.Практика.Знакомство с BI. Симончик.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Sales.SalesTerritory;
+SELECT TerritoryID, Name FROM Sales.SalesTerritory;
+SELECT * FROM Person.Person WHERE LastName='Norman';
+SELECT * FROM Person.Person WHERE EmailPromotion<>2;
+SELECT * FROM Person.Person WHERE NOT EmailPromotion=2;
+SELECT * FROM Person.Person WHERE EmailPromotion>2 OR EmailPromotion<2;
